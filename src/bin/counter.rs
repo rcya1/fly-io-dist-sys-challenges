b9ad126a8d0f40f57d1451c6232a8acf41eb24a4
Ryan Chang <chang.ryan10145@gmail.com>
2026-08-08 23:59:19 -0400

    Use the correct kv type for counter

--- src/bin/counter.rs
+++ src/bin/counter.rs
@@ -1,7 +1,7 @@
 use std::rc::Rc;
 
 use anyhow::{Result, anyhow, bail};
-use gossip::constants::LinKv;
+use gossip::constants::SeqKv;
 use gossip::kv::{CasStep, KvClient};
 use gossip::{App, Context, Message, Type, run};
 use serde_json::{Number, Value};
@@ -16,7 +16,7 @@ impl App for Counter {
     }
 
     async fn handle(&mut self, ctx: Rc<Context>, msg: Message) -> Result<()> {
-        let kv = KvClient::<LinKv>::new(ctx.clone());
+        let kv = KvClient::<SeqKv>::new(ctx.clone());
         match msg.type_ {
             Type::Add => {
                 let delta = msg
@@ -24,13 +24,19 @@ impl App for Counter {
                     .as_i64()
                     .ok_or_else(|| anyhow!("delta is not an integer"))?;
 
-                kv.cas_loop("counter", true, None, || Value::Number(Number::from(0)), |current| {
-                    let current = current
-                        .as_i64()
-                        .ok_or_else(|| anyhow!("counter value is not an integer"))?;
-                    let updated = Value::Number(Number::from(current + delta));
-                    Ok(CasStep::Apply(updated, ()))
-                })
+                kv.cas_loop(
+                    "counter",
+                    true,
+                    None,
+                    || Value::Number(Number::from(0)),
+                    |current| {
+                        let current = current
+                            .as_i64()
+                            .ok_or_else(|| anyhow!("counter value is not an integer"))?;
+                        let updated = Value::Number(Number::from(current + delta));
+                        Ok(CasStep::Apply(updated, ()))
+                    },
+                )
                 .await
                 .map_err(|e| anyhow!("cas on counter failed: {e}"))?;
 
